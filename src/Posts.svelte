ee1e6e458ccf123e8f0a48230ba0ce6bf5d10c21
Thomas Eberwein <thomas@thomastraum.com>
2020-06-24 22:01:23 +0100

    oembed fuckup

--- src/Posts.svelte
+++ src/Posts.svelte
@@ -6,6 +6,7 @@ import {getQueryString} from './helpers.js';
 import PostImage        from './posts/PostImage.svelte';
 import PostMarkdown     from './posts/PostMarkdown.svelte';
 import PostMedia        from './posts/PostMedia.svelte';
+import PostLink        from './posts/PostLink.svelte';
 
 // const channelslug = "random-access-memory";
 const channelslug = "blog-eqyzzcqjsfe";
@@ -21,7 +22,7 @@ let posts = getPosts();
 
 async function getPosts() {
   const url = `https://api.are.na/v2/channels/${channelslug}/contents?${getQueryString(params)}`
-  const res = await fetch(url)
+  const res = await fetch(url);
   const posts = await res.json();
   console.log(posts)
   return posts;
@@ -47,6 +48,10 @@ async function getPosts() {
         <h1 class="my-4">{@html post.title}</h1>
         <hr />
         <PostMedia {post} />
+      {:else if post.class == "Link" }
+        <h1 class="my-4">{@html post.title}</h1>
+        <hr />
+        <PostLink {post} />
       {:else}
         <h1 class="my-4">{@html post.title}</h1>
         <hr />
---
+++ src/posts/PostLink.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let post;
+
+  import {getQueryString} from '../helpers.js';
+
+  let twitterEmbed  = getOembedTwitter();
+
+  async function getOembedTwitter() {
+    console.log(post.source.url);
+    const url = `https://publish.twitter.com/oembed?${getQueryString({url:post.source.url})} `
+    console.log(url);
+    const res = await fetch(url);
+    console.log(res);
+    return res.html;
+  }
+
+</script>
+
+<style>
+
+.post-image {
+  float: left;
+}
+
+.blockquote {
+
+}
+
+</style>
+asdasdas
+
+<!-- <div class="post my-5"> -->
+{#await twitterEmbed}
+  ...loading
+{:then value}
+  {value}
+{/await}
+
+<!-- </div> -->
